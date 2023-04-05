5d688c46c7a86636a870e353387439026e2e1303
michaelPicilli <113585689+michaelPicilli@users.noreply.github.com>
2023-04-05 19:56:41 +0200

    Commit pagine esercizi

---
+++ Esercizio.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        <link rel="stylesheet" href="Style.css">
+        
+        <title>ESERCIZO</title> <!-- TODO: Nome esercizio -->
+    </head>
+
+    <header>
+        <?php
+            include 'navbar.html';
+        ?>
+    </header>
+
+    <body>
+        <h1>ESERCIZO</h1> <!-- TODO: Nome esercizio -->
+
+        <iframe height="500" width="900" src="https://www.youtube.com/embed/DbAuRVxT-F0" title="What Language is Spoken in Italy? and Why We Should Care" frameborder="0" allow="autoplay encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        
+        <h2>
+            <?php
+                //TODO: Query titolo dell'esercizio
+            ?>
+        </h2>
+
+        <iframe width="560" height="315" src="" title="Senna 1988" frameborder="0"  
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> <!-- TODO: Query link dell'esercizio -->
+    </body>
+
+    <footer>
+        <?php
+            include 'footer.html';
+        ?>
+    </footer>
+</html>
